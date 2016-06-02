5c4cadab4c386063c00ad664bf1b8a957465cf7b
holyroti <rohit_paltoe@live.nl>
2016-06-02 20:46:20 +0200

    a

--- Mooie naam/src/Controller/Main.java
+++ Mooie naam/src/Controller/Main.java
@@ -8,7 +8,9 @@ public class Main{
 		MainWindow window = new MainWindow();
 		window.setVisible(true);
 		window.setSize(300, 300);
-		window.setDefaultCloseOperation(0);
+		window.setDefaultCloseOperation(1);
 		window.setLocationRelativeTo(null);
+		
+		Database db = new Database("jdbc:mysql://localhost/world", "root", "qwerty123");
 	}
 }

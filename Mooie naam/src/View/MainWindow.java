e24d649788bca1b319b7dcea035faab9d0ed42e5
holyroti <rohit_paltoe@live.nl>
2016-06-02 12:10:32 +0200

    Changes to be committed: 	modified:   Mooie naam/.classpath 	modified:   Mooie naam/bin/.gitignore 	modified:   Mooie naam/src/View/MainWindow.java  Untracked files: 	Mooie naam/lib/ 	Mooie naam/src/View/GegevensOpvragen.java 	Mooie naam/src/View/OpleidingenOpzoeken.java 	Mooie naam/src/View/StudentInvoer.java 	Mooie naam/src/View/StudentenOpzoeken.java

--- Mooie naam/src/View/MainWindow.java
+++ Mooie naam/src/View/MainWindow.java
@@ -1,5 +1,48 @@
 package View;
 
-public class MainWindow {
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JSplitPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+public class MainWindow extends JFrame{
+	
+	public MainWindow(){
+		getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		getContentPane().add(splitPane);
+		
+		JPanel panel = new JPanel();
+		splitPane.setLeftComponent(panel);
+		panel.setLayout(new GridLayout(5, 1, 0, 0));
+		
+		JButton btnExcStuIn = new JButton("Buitenlandse student invoeren");
+		btnExcStuIn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnExcStuIn.setToolTipText("Klik om een nieuwe buitenlandse student met zijn gegevens in te voeren");
+		panel.add(btnExcStuIn);
+		
+		JButton btnHhsStuIn = new JButton("Binnenlandse student invoeren");
+		btnHhsStuIn.setToolTipText("Klik om een nieuwe binnenlandse student met zijn gegevens in te voeren");
+		panel.add(btnHhsStuIn);
+		
+		JButton btnStuSearch = new JButton("Student opzoeken");
+		btnStuSearch.setToolTipText("Klik om een student te zoeken zodat je deze kunt: wijzigen, inschrijven, bekijken of vinden");
+		panel.add(btnStuSearch);
+		
+		JButton btnOplSearch = new JButton("Opleiding opzoeken");
+		btnOplSearch.setToolTipText("Klik om een opleiding op te zoeken zodat je er een kunt toevoegen of bekijken");
+		panel.add(btnOplSearch);
+		
+		JButton btnGegOpvragen = new JButton("Gegevens opvragen");
+		btnGegOpvragen.setToolTipText("Klik om verschillende overzichten te kunnen bekijken");
+		panel.add(btnGegOpvragen);
+		
+	}
 }

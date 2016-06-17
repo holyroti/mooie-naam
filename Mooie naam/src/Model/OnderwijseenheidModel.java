e09a10fa1b9e0000c3be781785f7a40c18d9e472
holyroti <rohit_paltoe@live.nl>
2016-06-17 19:25:15 +0200

    cvf

---
+++ Mooie naam/src/Model/OnderwijseenheidModel.java
@@ -0,0 +1,42 @@
+package Model;
+
+public class OnderwijseenheidModel {
+	private int id;
+	private String type;
+	private int punten;
+	
+	public OnderwijseenheidModel(int id, String type, int punten) {
+		this.id = id;
+		this.type = type;
+		this.punten = punten;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getPunten() {
+		return punten;
+	}
+
+	public void setPunten(int punten) {
+		this.punten = punten;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return type;
+	}
+}
